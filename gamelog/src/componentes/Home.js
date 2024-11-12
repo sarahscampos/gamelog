92da97d89a9c8bec10b6e78f4a11b1166f5e81cc
sarah-campos <sarahstephanycampos@gmail.com>
2024-11-12 02:10:25 -0300

    feat: header + footer + pagina jogo

--- gamelog/src/componentes/Home.js
+++ gamelog/src/componentes/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import Header from './Header';
 
-const Home = () => {
+export const Home = () => {
   return (
     <>
+    <Header/>
     <h1 className="text-3xl font-bold underline">Home</h1>
     <Link to="/jogo">Jogo</Link>
     </>
