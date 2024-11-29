bacfc6af4e2adf8a8974abde56be76a54aa39498
Renato Alves <renato.alvs1103@gmail.com>
2024-11-28 22:20:37 -0300

    adição de página de forum

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import jogosReducer from './slices/jogosSlice';
 import avaliacoesReducer from './slices/avaliacoesSlice';
 import listasReducer from './slices/listasSlice';
+import forumReducer from './slices/forumSlice';
 
 const store = configureStore({
   reducer: {
     jogos: jogosReducer,
     avaliacoes: avaliacoesReducer,
     listas: listasReducer,
+    forum: forumReducer,
   },
 });
 
