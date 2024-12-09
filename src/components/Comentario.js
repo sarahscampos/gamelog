075ac9d74017763ae4ea4b7ad2d68d3b7bea01d2
Renato Alves de Sousa <renato.alvs1103@gmail.com>
2024-12-08 22:37:50 -0300

    Correção de horario dos comentarios no forum

--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,19 +1,26 @@
 // src/components/Comentario.js
 import React from 'react';
 
-const Comentario = ({ post}) => {
+const Comentario = ({ post }) => {
+
+  const dataHora = new Date(post.createdAt).toLocaleString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
   return (
     <div className="p-4 bg-white border border-gray-200 rounded-md shadow">
       <div className="flex items-center gap-3 mb-3">
-          <img src={"https://cdn.awsli.com.br/2657/2657807/produto/270496847/photo_5094015974415379628_x-4ucvc1908c.jpg"} alt={"Grimm"} className="w-10 h-10 rounded-full" />
-        
-        
+        <img src={"https://cdn.awsli.com.br/2657/2657807/produto/270496847/photo_5094015974415379628_x-4ucvc1908c.jpg"} alt={"Grimm"} className="w-10 h-10 rounded-full" />
         <div>
           <span className="font-semibold text-gray-800">{"Grimm" || 'Usuário Anônimo'}</span>
         </div>
       </div>
       <p>{post.content}</p>
-      <span className="text-sm text-gray-500">{new Date(post.createdAt).toLocaleString()}</span>
+      <span className="text-sm text-gray-500">{dataHora}</span>
     </div>
   );
 };
