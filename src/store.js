9e7d2759bb246149f6f8b49b2081769fe76ba913
ErichJohann <97558441+ErichJohann@users.noreply.github.com>
2024-12-10 19:05:00 -0300

    Exlusão arquivos inuteis

--- src/store.js
+++ src/store.js
@@ -4,7 +4,6 @@ import avaliacoesReducer from './slices/avaliacoesSlice';
 import listasReducer from './slices/listasSlice';
 import forumReducer from './slices/forumSlice';
 import cadastroReducer from './slices/cadastroSlice';
-import reviewReducer from './slices/reviewSlice';
 import usuarioReducer from './slices/usuarioSlice'
 
 const store = configureStore({
@@ -14,7 +13,6 @@ const store = configureStore({
     listas: listasReducer,
     forum: forumReducer,
     cadastro: cadastroReducer,
-    review: reviewReducer,
     perfil: usuarioReducer,
   },
 });
