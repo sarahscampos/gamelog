a75e373088a2cc16ad27a18e78a33fd339605e3d
JGK19 <106597685+JGK19@users.noreply.github.com>
2025-02-10 10:47:35 -0300

    acessar jogo pela lista
    _id

--- front-end/src/components/CarrosselEdit.js
+++ front-end/src/components/CarrosselEdit.js
@@ -50,7 +50,7 @@ const responsive = {
         <>
         <Carousel responsive={responsive} removeArrowOnDeviceType={["mobile"]} className='p-5'>
         {jogos.map((item, index) => (
-            <div key={index} className='w-40 drop-shadow-md cursor-pointer hover:scale-105 transition lg:w-56 flex flex-col items-center' onClick={() => navigate(`/jogo/${item.id}`)}>
+            <div key={index} className='w-40 drop-shadow-md cursor-pointer hover:scale-105 transition lg:w-56 flex flex-col items-center' onClick={() => navigate(`/jogo/${item._id}`)}>
             <img src={item.capa} alt="logoGamelog" className='w-40 h-52 mb-1 ring-solid ring-2 ring-indigo-600 rounded-sm lg:w-52 lg:h-72' />
             <p className='text-center font-fira text-base'>{item.nome}</p>
             <button
